f585860ac5dd138b7b77b4eb5acd3b20fb046145
ken <kenwebworkwatarai@gmail.com>
2024-04-11 14:38:57 +0900

    240411

--- includes/menu-nav.php
+++ includes/menu-nav.php
@@ -3,5 +3,5 @@
   <li><a href="<?php echo esc_url('/lesson'); ?>">レッスン一覧</a></li>
   <li><a href="<?php echo esc_url('/article'); ?>">記事一覧</a></li>
   <li><a href="<?php echo esc_url('/about'); ?>">サイトの使い方</a></li>
-  <li><a href="<?php echo esc_url('/landing'); ?>">daraEnglish の紹介</a></li>
+  <li><a href="<?php echo esc_url('/landing'); ?>"><span class="noto">daraEnglish</span> の紹介</a></li>
 </ul>
