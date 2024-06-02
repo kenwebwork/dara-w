44ae3f7083db57fc751cb83f79bb72e5c2f85477
kenwebwork <161683544+kenwebwork@users.noreply.github.com>
2024-06-02 11:32:47 +0900

    Add files via upload

---
+++ includes/steps/step5.php
@@ -0,0 +1,9 @@
+<h3 id="step5">Step 5</h3>
+<h4>Step 5-1 - 関係詞</h4>
+<?php
+  $lesson_array5_1 = array(
+    "who-that-which-of-relativepronoun" => "関係代名詞の who, that, which",
+  );
+  
+  get_template_part('includes/steps/lessonList', null, $lesson_array5_1);
+?>
