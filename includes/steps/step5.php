65ab8e31887103741b206daa466bbf5d52206f04
kenwebwork <161683544+kenwebwork@users.noreply.github.com>
2024-06-06 10:47:03 +0900

    Update step5.php

--- includes/steps/step5.php
+++ includes/steps/step5.php
@@ -6,6 +6,7 @@
     "relativepronoun-without-who-that-which" => "関係代名詞 who, that, which の省略",
     "what-of-relativepronoun" => "関係代名詞の what",
     "relativepronoun-whose-and-ofwhich" => "関係代名詞の whose と of which",
+    "where-when-why-how-of-relativeadverb" => "関係副詞の where, when, why, how",
   );
   
   get_template_part('includes/steps/lessonList', null, $lesson_array5_1);
